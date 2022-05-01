eeb3b5f9e64b312c2b029377b3f5c598f21ca6ad
luzianegering <93748581+luzianegering@users.noreply.github.com>
2022-05-01 09:43:17 -0300

    Delete comprasRoutes.js

--- src/routes/comprasRoutes.js
+++
@@ -1,9 +0,0 @@
-const { Router } = require("express");
-const comprasControllers = require("../compras/controller/compras");
-const router = Router();
-
-module.exports =
- router
-.post('/compra', comprasControllers.cadastrarCompra)
-.get('/compras', comprasControllers.listarTodasAsCompras)
-.get('/compra/:id', comprasControllers.listaCompraPorId)
