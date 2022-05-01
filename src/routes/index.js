4dbad4438ee2d479f3326b9a8fcaa08467766099
luzianegering <93748581+luzianegering@users.noreply.github.com>
2022-05-01 09:44:39 -0300

    Update index.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,7 @@
 const usuariosRoutes = require('./usuariosRoutes');
-const comprasRoutes = require("./comprasRoutes")
-const produtosFRoutes = require('./produtosFRoutes')
-const produtosMRoutes = require('./produtosMRoutes')
-const produtosLRoutes = require('./produtosLRoutes')
+
 
 const express = require('express')
 module.exports = (app) => {
-    app.use(usuariosRoutes, produtosFRoutes, produtosLRoutes, produtosMRoutes, comprasRoutes)
+    app.use(usuariosRoutes)
 };
